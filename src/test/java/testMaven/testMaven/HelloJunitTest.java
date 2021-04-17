6f380a7a5950ba1e4013e3a66d88f33b4ff97774
池田智耶 <tomoyaikeda@gmail.com>
2021-04-17 15:10:14 +0900

    共通化

--- src/test/java/testMaven/testMaven/HelloJunitTest.java
+++ src/test/java/testMaven/testMaven/HelloJunitTest.java
@@ -9,8 +9,7 @@ public class HelloJunitTest {
     @Test
     public void case1() throws Exception {
     	
-    	HelloJunit helloJunit = new HelloJunit();
-    	String message = helloJunit.stringJunit(2,2,2);
+    	String message = doExecute(2,2,2);
     	String expected = "全て同じです。";
     	assertEquals(message,expected);
     	
@@ -19,8 +18,7 @@ public class HelloJunitTest {
     @Test
     public void case2() throws Exception {
     	
-    	HelloJunit helloJunit = new HelloJunit();
-    	String message = helloJunit.stringJunit(5,3,1);
+    	String message = doExecute(5,3,1);
     	String expected = "これらの中で、最大値は5で、最小値は1です。";
     	assertEquals(message,expected);
     	
@@ -29,8 +27,7 @@ public class HelloJunitTest {
     @Test
     public void case3() throws Exception {
     	
-    	HelloJunit helloJunit = new HelloJunit();
-    	String message = helloJunit.stringJunit(-1,-3,-6);
+    	String message = doExecute(-1,-3,-6);
     	String expected = "これらの中で、最大値は-1で、最小値は-6です。";
     	assertEquals(message,expected);
     	
@@ -39,8 +36,7 @@ public class HelloJunitTest {
     @Test
     public void case4() throws Exception {
     	
-    	HelloJunit helloJunit = new HelloJunit();
-    	String message = helloJunit.stringJunit(0,-1,-2);
+    	String message = doExecute(0,-1,-2);
     	String expected = "これらの中で、最大値は0で、最小値は-2です。";
     	assertEquals(message,expected);
     	
@@ -49,8 +45,7 @@ public class HelloJunitTest {
     @Test
     public void case5() throws Exception {
     	
-    	HelloJunit helloJunit = new HelloJunit();
-    	String message = helloJunit.stringJunit(2,6,4);
+    	String message = doExecute(2,6,4);
     	String expected = "これらの中で、最大値は6で、最小値は2です。";
     	assertEquals(message,expected);
     	
@@ -59,8 +54,7 @@ public class HelloJunitTest {
     @Test
     public void case6() throws Exception {
     	
-    	HelloJunit helloJunit = new HelloJunit();
-    	String message = helloJunit.stringJunit(-6,-1,-2);
+    	String message = doExecute(-6,-1,-2);
     	String expected = "これらの中で、最大値は-1で、最小値は-6です。";
     	assertEquals(message,expected);
     	
@@ -69,8 +63,7 @@ public class HelloJunitTest {
     @Test
     public void case7() throws Exception {
     	
-    	HelloJunit helloJunit = new HelloJunit();
-    	String message = helloJunit.stringJunit(-1,6,3);
+    	String message = doExecute(-1,6,3);
     	String expected = "これらの中で、最大値は6で、最小値は-1です。";
     	assertEquals(message,expected);
     	
@@ -79,8 +72,7 @@ public class HelloJunitTest {
     @Test
     public void case8() throws Exception {
     	
-    	HelloJunit helloJunit = new HelloJunit();
-    	String message = helloJunit.stringJunit(3,1,6);
+    	String message = doExecute(3,1,6);
     	String expected = "これらの中で、最大値は6で、最小値は1です。";
     	assertEquals(message,expected);
     	
@@ -89,8 +81,7 @@ public class HelloJunitTest {
     @Test
     public void case9() throws Exception {
     	
-    	HelloJunit helloJunit = new HelloJunit();
-    	String message = helloJunit.stringJunit(-2,-6,-1);
+    	String message = doExecute(-2,-6,-1);
     	String expected = "これらの中で、最大値は-1で、最小値は-6です。";
     	assertEquals(message,expected);
     	
@@ -99,8 +90,7 @@ public class HelloJunitTest {
     @Test
     public void case10() throws Exception {
     	
-    	HelloJunit helloJunit = new HelloJunit();
-    	String message = helloJunit.stringJunit(8,-1,11);
+    	String message = doExecute(8,-1,11);
     	String expected = "これらの中で、最大値は11で、最小値は-1です。";
     	assertEquals(message,expected);
     	
@@ -109,8 +99,7 @@ public class HelloJunitTest {
     @Test
     public void case11() throws Exception {
     	
-    	HelloJunit helloJunit = new HelloJunit();
-    	String message = helloJunit.stringJunit(3,3,1);
+    	String message = doExecute(3,3,1);
     	String expected = "これらの中で、最大値は3で、最小値は1です。";
     	assertEquals(message,expected);
     	
@@ -119,8 +108,7 @@ public class HelloJunitTest {
     @Test
     public void case12() throws Exception {
     	
-    	HelloJunit helloJunit = new HelloJunit();
-    	String message = helloJunit.stringJunit(2,2,4);
+    	String message = doExecute(2,2,4);
     	String expected = "これらの中で、最大値は4で、最小値は2です。";
     	assertEquals(message,expected);
     	
@@ -129,8 +117,7 @@ public class HelloJunitTest {
     @Test
     public void case13() throws Exception {
     	
-    	HelloJunit helloJunit = new HelloJunit();
-    	String message = helloJunit.stringJunit(3,1,3);
+    	String message = doExecute(3,1,3);
     	String expected = "これらの中で、最大値は3で、最小値は1です。";
     	assertEquals(message,expected);
     	
@@ -139,8 +126,7 @@ public class HelloJunitTest {
     @Test
     public void case14() throws Exception {
     	
-    	HelloJunit helloJunit = new HelloJunit();
-    	String message = helloJunit.stringJunit(2,4,2);
+    	String message = doExecute(2,4,2);
     	String expected = "これらの中で、最大値は4で、最小値は2です。";
     	assertEquals(message,expected);
     	
@@ -149,8 +135,7 @@ public class HelloJunitTest {
     @Test
     public void case15() throws Exception {
     	
-    	HelloJunit helloJunit = new HelloJunit();
-    	String message = helloJunit.stringJunit(1,3,3);
+    	String message = doExecute(1,3,3);
     	String expected = "これらの中で、最大値は3で、最小値は1です。";
     	assertEquals(message,expected);
     	
@@ -159,11 +144,18 @@ public class HelloJunitTest {
     @Test
     public void case16() throws Exception {
     	
-    	HelloJunit helloJunit = new HelloJunit();
-    	String message = helloJunit.stringJunit(4,2,2);
+    	String message = doExecute(4,2,2);
     	String expected = "これらの中で、最大値は4で、最小値は2です。";
     	assertEquals(message,expected);
     	
+    }
+    
+    public String doExecute(Integer num1, Integer num2, Integer num3) {
+    	
+    	HelloJunit helloJunit = new HelloJunit();
+    	String message = helloJunit.stringJunit(num1,num2,num3);
+    	
+    	return message;
     }
     
 }
